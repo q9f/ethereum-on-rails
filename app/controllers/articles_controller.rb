8ed129b642247013fc94b6b40b356554d97e7259
Afr Schoe <58883403+q9f@users.noreply.github.com>
2021-08-31 13:35:47 +0200

    articles: implement destroy controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -34,6 +34,12 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to root_path
+  end
+
   private
     def article_params
       params.require(:article).permit(:title, :body)
