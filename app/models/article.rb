e5b5d0c53d7f6b60f55aa2dae98f7f54b0fc3a8b
Afr Schoe <58883403+q9f@users.noreply.github.com>
2021-08-31 14:06:42 +0200

    model: define article visibility

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,11 @@ class Article < ApplicationRecord
 
   validates :title, presence: true
   validates :body, presence: true, length: {minimum: 32}
+
+  VALID_STATUSES = ['public', 'private', 'archived']
+  validates :status, inclusion: { in: VALID_STATUSES }
+
+  def archived?
+    status == 'archived'
+  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,10 @@
 class Comment < ApplicationRecord
   belongs_to :article
+
+  VALID_STATUSES = ['public', 'private', 'archived']
+  validates :status, inclusion: { in: VALID_STATUSES }
+
+  def archived?
+    status == 'archived'
+  end
 end
