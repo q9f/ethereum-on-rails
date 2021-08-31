dea8e9808c545caa73d67bf7de36941d5f20b949
Afr Schoe <58883403+q9f@users.noreply.github.com>
2021-08-31 13:29:48 +0200

    views: create partial from for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,6 +21,19 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
+
   private
     def article_params
       params.require(:article).permit(:title, :body)
