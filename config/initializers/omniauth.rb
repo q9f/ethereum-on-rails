0f9e61419c12b762e76e7fd6f6060c159ba03d4a
Afr Schoe <58883403+q9f@users.noreply.github.com>
2021-09-01 12:00:23 +0200

    config: add comments to existing logic

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+  # omniauth github provider
+  # @TODO: @q9f setup credentials
   provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/login'
-  get 'sessions/welcome'
-  get 'users/new'
-  get 'users/create'
+  # default to articles index
   root "articles#index"
 
+  # omniauth github authentication routes
   get "/auth/github", as: "github_login"
   get "/auth/:provider/callback", to: "sessions#update"
 
+  # users and sessions routes
+  resources :users
+  resources :sessions
+
+  # articles and comments (nested) routes
   resources :articles do
     resources :comments
   end
