86a335f72b875423fc227c75fb2789d720f375dc
Afr Schoe <58883403+q9f@users.noreply.github.com>
2021-09-01 12:42:13 +0200

    models: validate users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   # macro for bcrypt methods
   has_secure_password
+
+  # user requires name and password (digest)
+  validates :name, presence: true
+  validates :digest, presence: true
 end
