85e2eb6ad1c018da6e36f7af5166d673ce380f69
Afr Schoe <58883403+q9f@users.noreply.github.com>
2021-09-24 15:12:14 +0200

    app/controller: user requires a name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,41 +13,50 @@ class UsersController < ApplicationController
     # create new user from allowed params
     @user = User.new(user_params)
 
-    # create randome nonce
-    @user.eth_nonce = SecureRandom.uuid
+    # only proceed with pretty names
+    if @user and @user.username and @user.username.size > 0
 
-    # usually, we want to let the user sign the nonce to make sure
-    # they are authorized to create a new user account for the
-    # given ethereum address. for the demo, we skip this for the
-    # sake of keeping it simple.
+      # create randome nonce
+      @user.eth_nonce = SecureRandom.uuid
 
-    # only proceed with eth address
-    if @user.eth_address
+      # usually, we want to let the user sign the nonce to make sure
+      # they are authorized to create a new user account for the
+      # given ethereum address. for the demo, we skip this for the
+      # sake of keeping it simple.
 
-      # make sure the eth address is valid
-      address = Eth::Address.new @user.eth_address
-      if address.valid?
+      # only proceed with eth address
+      if @user.eth_address
 
-        # save to database
-        if @user.save
+        # make sure the eth address is valid
+        address = Eth::Address.new @user.eth_address
+        if address.valid?
 
-          # if user is created, congratulations, send them to login
-          redirect_to login_path, notice: 'Successfully created an account, you may now log in.'
+          # save to database
+          if @user.save
+
+            # if user is created, congratulations, send them to login
+            redirect_to login_path, notice: 'Successfully created an account, you may now log in.'
+          else
+
+            # if it fails, eth address is already in database, go to login
+            redirect_to login_path, alert: "Account already exists! Try to log in instead!"
+          end
         else
 
-          # if it fails, eth address is already in database, go to login
-          redirect_to login_path, alert: "Account already exists! Try to log in instead!"
+          # if it fails, the eth address is not valid, stay on signup
+          flash.now[:alert] = "Invalid Ethereum address!"
+          render :new
         end
       else
 
-        # if it fails, the eth address is not valid, stay on signup
-        flash.now[:alert] = "Invalid Ethereum address!"
+        # if it fails, the process was interupted, stay on signup
+        flash.now[:alert] = "Failed to get Ethereum address!"
         render :new
       end
     else
 
-      # if it fails, the process was interupted, stay on signup
-      flash.now[:alert] = "Failed to get Ethereum address!"
+      # if it fails, the user did not specify a name, stay on signup
+      flash.now[:alert] = "Please choose a name (length > 0)!"
       render :new
     end
   end
@@ -55,6 +64,6 @@ class UsersController < ApplicationController
   private
   def user_params
     # only allow user to control name, message, signature, and address
-    params.require(:user).permit(:username, :eth_message, :eth_address, :eth_signature)
+    params.require(:user).permit(:username, :eth_address)
   end
 end
