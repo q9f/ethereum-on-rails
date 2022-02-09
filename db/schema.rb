17e1854aab96a0abb021e14942f51aee9845b29d
/raw PONG _GHMoaCXLT <58883403+q9f@users.noreply.github.com>
2022-02-09 18:28:24 +0100

    gem: update rails to 7.0

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_23_091236) do
-
+ActiveRecord::Schema[7.0].define(version: 2021_09_23_091236) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "eth_address"
     t.string "eth_nonce"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
